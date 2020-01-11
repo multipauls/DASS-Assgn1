0985d487637f00e4f045cecbba5c67e936ccd3fa
Shubhi Dutta <shubhangi.dutta@research.iiit.ac.in>
2020-01-11 14:49:12 +0530

    bullets

--- try.py
+++ try.py
@@ -41,7 +41,7 @@ def inputChar():
 		''' input method '''
 		raise AlarmException
 
-	def userInput(timeout=0.1):
+	def userInput(timeout=0.2):
 		''' input method '''
 		signal.signal(signal.SIGALRM, alarmHandler)
 		signal.setitimer(signal.ITIMER_REAL, timeout)
@@ -70,7 +70,7 @@ def terminalSize():
 	return w, h
 
 
-def background(y, x):
+def background():
 		print (' '*terminalSize()[0])
 
 		print ('-'*terminalSize()[0])
@@ -84,10 +84,7 @@ def background(y, x):
 
 
 
-def gravity(y,val):
-	if val!='w' and y<terminalSize()[1]-4:
-		y+=1
-	return y
+
 
 
 
@@ -120,49 +117,106 @@ class bgCoin(bgObject):
 
 
 
+class charObject:
+	def __init__(self):
+		self.y=terminalSize()[1]-3
+		self.x=7		
+
+	def renderObject(self):
+		print ('\033['+ str(self.y) +';' + str(self.x) + 'H ' + str(self.x)+str(self.y)+' '+str(terminalSize()[1]))
+
+
+
+
+
+
+class dinObject(charObject):
+	def __init__(self):
+		
+		super().__init__()
+
+	def gravity(self,val):#,gravCount):
+		if val!='w' and self.y<terminalSize()[1]-2: #and gravCount==1:
+			self.y+=1
+		#gravCount= not(gravCount)
+		return self.y#, gravCount
+
+	def getXY(self):
+		return self.x, self.y
+
+	def moveDin(self, val):
+		if (val=='w') and self.y>3:
+					self.y-=1
+		elif (val=='a' and self.x>0):
+					self.x-=1
+		elif (val=='d' and self.x<terminalSize()[0]):
+					self.x+=1
+		self.gravity(val)
+
+
+class flyingObject():
+	def __init__(self,x,y):
+		self.x=x	
+		self.y=y
+
+	def moveAcross(self):
+		if self.x<terminalSize()[0] and self.x!=-1:
+			self.x+=2
+		else:
+			self.x= -1
+
+	def getXY(self):
+		return self.x, self.y
+
+	def renderObject(self):
+		self.moveAcross()
+		if self.x!=-1:
+			print ('\033['+ str(self.y) +';' + str(self.x) + 'H >')
 
 
 def mainGame():
-	y=terminalSize()[1]-3
-	x=7
+	
+	gravCount=0
 	coinList=[]
-	while True:
-			time.sleep(0.1)
-			background(y,x)
-			if (y<terminalSize()[1]-3):
-				y=terminalSize()[1]-3
-			print ('\033['+ str(y) +';' + str(x) + 'H ' + str(x)+str(y)+' '+str(terminalSize()[1]))
-			
-			#f = open("demofile.txt", "a")
+	bulletList=[]
+	Din= dinObject()
 
+	while True:
+			time.sleep(0.02)
+			background()
+			
+			Din.renderObject()
 
 			prob=np.random.uniform()
-			
 
-
-			if(prob>=0.9):
+			if(prob>=0.95):
 				coin=bgCoin()
 				coinList.append(coin)
-#			f.write("\n"+str(len(coinList))+"\n")
 
 			for i in range(len(coinList)):
 				coinList[i].renderObject()
-#				f.write(str(coinList[i].getY())+"\n")
-#			f.close()
+				
+
+			for i in range(len(bulletList)):
+				bulletList[i].renderObject()
+				
+
+
 
 
 			val=inputChar()
 
-			if (val=='w' and y<terminalSize()[1]-1):
-					y-=1
-			elif (val=='a' and x>0):
-					x-=1
-			elif (val=='d' and x<terminalSize()[0]):
-					x+=1
-			elif (val=='c'):
+			
+			if (val=='c'):
 				break
-			y=gravity(y,val)
-		
+			elif (val=='b'):
+				x,y=Din.getXY()
+				bullet=flyingObject(x,y)
+				bulletList.append(bullet)
+			
+			else:
+				Din.moveDin(val)
+			
 			
 
 			
